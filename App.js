3b254c1672f9fd646170dc773ecc115113dbca9c
KaanKayserili <knkayserili@gmail.com>
2023-07-12 11:19:18 +0300

    edit

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
+import { FontAwesome5, Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tenDay from './assets/tenDay';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{ color: "#FFF", textAlign: "center", fontSize: 28, fontWeight: "700" }}>KK WEATHER</Text>
+
+      <View style={{ backgroundColor: '#11ceeb', borderRadius: 20, padding: 20, marginTop: 20, width: "90%", }}>
+        <Text style={{ fontSize: 24, fontWeight: "700", color: "white", textAlign: "center" }}>Bakırköy</Text>
+
+        <View style={{ flexDirection: "row", justifyContent: "space-around", marginTop: 20 }}>
+          <Ionicons name={"sunny-outline"} size={100} color={"yellow"} />
+          <View>
+            <Text style={{ fontSize: 32, fontWeight: "700", color: "white", textAlign: "center" }}>28°C</Text>
+            <View>
+              <Text style={{ fontSize: 14, fontWeight: "600", color: "white", textAlign: "center" }}>Düşük: 23°C</Text>
+              <Text style={{ fontSize: 14, fontWeight: "600", color: "white", textAlign: "center" }}>Yüksek: 30°C</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View style={{ backgroundColor: '#11ceeb', borderRadius: 20, padding: 20, marginTop: 20, width: "90%" }}>
+        <Text style={{ color: "#ccceeb", fontSize: 14, fontWeight: "500" }}>10 Günlük Tahmin</Text>
+
+        {tenDay?.map((item, index) => {
+          return (
+            <View key={index} style={{ flexDirection: "row", justifyContent: "space-between", width: "80%" }}>
+              <Text style={{ fontSize: 20, fontWeight: "500", color: "white" }}>{item.text.slice(0, 3)}</Text>
+              {item.image == "wind" ?
+                <FontAwesome5 name={"wind"} size={20} color={"white"} />
+                : <Ionicons name={item.image} size={20} color={item.image == "sunny-outline" ? "yellow" : "white"} />
+              }
+              <Text style={{ fontSize: 20, fontWeight: "500", color: "lightblue" }}>{item.low}</Text>
+              <Text style={{ fontSize: 20, fontWeight: "500", color: "white" }}>{item.high}</Text>
+            </View>
+          )
+        })
+        }
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 40,
-    backgroundColor: '#212121',
+    backgroundColor: '#87ceeb',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
---
+++ assets/tenDay.js
@@ -0,0 +1,16 @@
+
+
+const tenDay = [
+    { text: "Çarşamba", image: "sunny-outline", low: "23", high: "30" },
+    { text: "Perşembe", image: "sunny-outline", low: "23", high: "32" },
+    { text: "Cuma", image: "sunny-outline", low: "23", high: "33" },
+    { text: "Cumartesi", image: "rainy-outline", low: "23", high: "31" },
+    { text: "Pazar", image: "sunny-outline", low: "23", high: "29" },
+    { text: "Pazartesi", image: "wind", low: "21", high: "29" },
+    { text: "Salı", image: "wind", low: "21", high: "29" },
+    { text: "Çarşamba", image: "sunny-outline", low: "21", high: "29" },
+    { text: "Perşembe", image: "sunny-outline", low: "21", high: "29" },
+    { text: "Cuma", image: "sunny-outline", low: "21", high: "30" },
+];
+
+export default tenDay;
